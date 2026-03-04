a55303a8b5d5d74bf29086f9695de60a31a8370f
NayeliMorales19 <moralesnayeli120217@gmail.com>
2026-03-04 11:19:22 -0600

    se actualio app.js para la autenticacion, navegacion, etc.

--- react-login-page/src/App.js
+++ react-login-page/src/App.js
@@ -1,24 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+
+import Navigation from './components/Navigation';
+import Login from './components/Login';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Navigation />
+
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                  </p>
+                }
+              />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
